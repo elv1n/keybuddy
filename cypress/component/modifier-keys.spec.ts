7212626392b6c6fe55a75c8dcda2d8a7d9f9d813
Elvin Dzhavadov <elvin.javadov@pm.me>
2026-01-14 14:21:35 -0800

    Test modifier regression

---
+++ cypress/component/modifier-keys.spec.ts
@@ -0,0 +1,25 @@
+import { createKeybuddy } from '../../src/keybuddy';
+
+describe('Modifier key detection', () => {
+  let kb: ReturnType<typeof createKeybuddy>;
+
+  beforeEach(() => {
+    kb = createKeybuddy(document, () => true);
+  });
+
+  afterEach(() => {
+    kb.destroy();
+  });
+
+  it('should trigger cmd+a shortcut (Meta key must not pollute downKeys)', () => {
+    const handler = cy.stub().as('handler');
+    kb.bind('cmd+a', handler);
+
+    cy.document().then((doc) => {
+      doc.dispatchEvent(new KeyboardEvent('keydown', { key: 'Meta', metaKey: true, bubbles: true }));
+      doc.dispatchEvent(new KeyboardEvent('keydown', { key: 'a', metaKey: true, bubbles: true }));
+    });
+
+    cy.get('@handler').should('have.been.calledOnce');
+  });
+});
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,19 +1,1 @@
 import './commands';
-// Custom mount command for plain HTML/DOM testing
-const mount = (template: string | (() => string)) => {
-  const html = typeof template === 'function' ? template() : template;
-
-  return cy.get('body').then(($body) => {
-    $body.html(html);
-  });
-};
-
-Cypress.Commands.add('mount', mount);
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
